b7918970654b23ebf7fb1e8d4f421f03c583d4db
Hirash <hirashm32@gmail.com>
2024-03-15 18:13:43 +0330

    fix Data Grid

--- Computation.UI/Forms/Person/PersonTypesForm.Designer.cs
+++ Computation.UI/Forms/Person/PersonTypesForm.Designer.cs
@@ -28,11 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "PersonTypesForm";
+            components = new System.ComponentModel.Container();
+            gridControl = new DevExpress.XtraGrid.GridControl();
+            gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            AddRow = new DevExpress.XtraEditors.SimpleButton();
+            DeleteRow = new DevExpress.XtraEditors.SimpleButton();
+            personTypeAppBindingSource = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)gridControl).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)gridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)personTypeAppBindingSource).BeginInit();
+            SuspendLayout();
+            // 
+            // gridControl
+            // 
+            gridControl.DataSource = personTypeAppBindingSource;
+            gridControl.Dock = DockStyle.Bottom;
+            gridControl.Location = new Point(0, 31);
+            gridControl.MainView = gridView1;
+            gridControl.Name = "gridControl";
+            gridControl.RightToLeft = RightToLeft.Yes;
+            gridControl.Size = new Size(351, 319);
+            gridControl.TabIndex = 0;
+            gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] { gridView1 });
+            // 
+            // gridView1
+            // 
+            gridView1.GridControl = gridControl;
+            gridView1.Name = "gridView1";
+            gridView1.OptionsView.NewItemRowPosition = DevExpress.XtraGrid.Views.Grid.NewItemRowPosition.Bottom;
+            // 
+            // AddRow
+            // 
+            AddRow.Location = new Point(0, 2);
+            AddRow.Name = "AddRow";
+            AddRow.Size = new Size(75, 23);
+            AddRow.TabIndex = 1;
+            AddRow.Text = "اضافه کردن";
+            AddRow.Click += AddRow_Click;
+            // 
+            // DeleteRow
+            // 
+            DeleteRow.Location = new Point(81, 2);
+            DeleteRow.Name = "DeleteRow";
+            DeleteRow.Size = new Size(75, 23);
+            DeleteRow.TabIndex = 2;
+            DeleteRow.Text = "حذف";
+            // 
+            // personTypeAppBindingSource
+            // 
+            personTypeAppBindingSource.DataSource = typeof(Application.PersonTypeApp);
+            // 
+            // PersonTypesForm
+            // 
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(351, 350);
+            Controls.Add(DeleteRow);
+            Controls.Add(AddRow);
+            Controls.Add(gridControl);
+            MaximizeBox = false;
+            Name = "PersonTypesForm";
+            Text = "PersonTypesForm";
+            Shown += PersonTypesForm_Load;
+            ((System.ComponentModel.ISupportInitialize)gridControl).EndInit();
+            ((System.ComponentModel.ISupportInitialize)gridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)personTypeAppBindingSource).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraEditors.SimpleButton AddRow;
+        private DevExpress.XtraEditors.SimpleButton DeleteRow;
+        private BindingSource personTypeAppBindingSource;
     }
 }
--- Computation.UI/Forms/Person/PersonTypesForm.cs
+++ Computation.UI/Forms/Person/PersonTypesForm.cs
@@ -1,13 +1,15 @@
-﻿using DevExpress.XtraEditors;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using Application.Contracts.PersonType;
+using DevExpress.CodeParser;
+using DevExpress.XtraCharts;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using Infrastructure.Models;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Controls;
+using DevExpress.XtraEditors;
+using DevExpress.Mvvm.Native;
+using DevExpress.Utils.Extensions;
+
 
 namespace Computation.UI.Forms.Person
 {
@@ -16,6 +18,59 @@ namespace Computation.UI.Forms.Person
         public PersonTypesForm()
         {
             InitializeComponent();
+
+           
+           
         }
+
+       
+
+      
+
+        private void PersonTypesForm_Load(object sender, EventArgs e)
+        {
+            using (var unit = new UnitOfWork())
+            {
+                var personTypes = unit.PersonTypeApp.GetPersonTypes();
+
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Id", typeof(int));
+                dt.Columns.Add("Name", typeof(string));
+
+                foreach (var personType in personTypes)
+                {
+                    dt.Rows.Add(personType.Id, personType.Name);
+                }
+
+                gridControl.DataSource = dt;
+            }
+        }
+
+
+
+        private void AddRow_Click(object sender, EventArgs e)
+        {
+
+            DataTable added = gridControl.DataSource as DataTable;
+            
+            foreach (DataRow row in added.Rows)
+            {
+
+                using (var unit = new UnitOfWork())
+                {
+                    if (row.RowState == DataRowState.Added)
+                    {
+                        var personType = new PersonType() { Name = row["Name"].ToString() };
+                        unit.PersonTypeApp.AddPersonType(personType);
+                    }
+                }
+            }
+            PersonTypesForm_Load(sender, e);
+        }
+
+
+
+
+
     }
 }
